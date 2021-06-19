627a605d0a11765d6a8347c56c170de03bade038
Camilo Carvajal <camilo@talent.com>
2021-06-19 15:06:47 -0500

    Basic Login

---
+++ POSUNO/POSUNO/POSUNO.Shared/Pages/LoginPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using POSUNO.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace POSUNO.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+ 
+        private async void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = await ValidForm();
+            if (!isValid) {
+                return;
+            }
+            MessageDialog messageDialog = new MessageDialog("Vamos bien", "OK");
+            await messageDialog.ShowAsync();
+
+        }
+
+        private async Task<bool> ValidForm()
+        {
+            MessageDialog messageDialog;
+
+            if (string.IsNullOrEmpty(TextEmail.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (!RegexUtilities.IsValidEmail(TextEmail.Text))
+            {
+                messageDialog = new MessageDialog("Debes ingresar un email valido", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtPassword.Password))
+            {
+                messageDialog = new MessageDialog("Debes ingresar tu email", "Error");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
